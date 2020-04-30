b944792ef7b923e735b64ac5afffe9a2a33a0ede
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2020-04-30 06:56:07 +0200

    fix: allow qovery hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << '.qovery.io'
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
