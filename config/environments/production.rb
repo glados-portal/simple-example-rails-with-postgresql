8749bfd0c0007819df795328cc3dbad3e50b5cf7
glados-portal <91133531+glados-portal@users.noreply.github.com>
2021-09-21 17:03:19 +0200

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << '.qovery.io'
+  config.hosts << 'z780a66ad-zeb1e41ca-gtw.slab.sh'
   
   # Code is not reloaded between requests.
   config.cache_classes = true
